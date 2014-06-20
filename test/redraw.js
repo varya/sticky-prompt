b7c8306b5254f8dfdafaa3c9eab3b14b8a201f74
Varya Stepanova <mail@varya.me>
2014-06-20 16:47:58 +0400

    With delimeter line

---
+++ test/redraw.js
@@ -0,0 +1,30 @@
+var prompt = new (require('../lib/sticky-prompt.js'))();
+var chalk = require('chalk');
+
+prompt.prompt.message = chalk.cyan("Varya") + ' ' + chalk.magenta("StickyPrompt");
+
+prompt.get([
+    {
+        name: 'cont',
+        message: 'countinue? [number/n]'
+    }
+    ], function(err, res) {
+        if (res.cont == 'n') {
+            prompt.stop();
+            return;
+        }
+        showCount(res.cont);
+    },
+    {
+        next: 'cycle',
+        redraw: "Thank for typing! \n",
+    }
+);
+
+var showCount = function(n) {
+    for (var i = 0; i < n; i++) (function(i) {
+        setTimeout(function() {
+            prompt.log(i);
+        }, i*1000);
+    })(i);
+}
